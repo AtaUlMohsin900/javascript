0b072ed465f4ddbc7b8a6b93aa708670d873f59b
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-10-22 05:28:55 +0000

    dataTypes

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,9 @@
+let UserName =  "khalid"; // string 2 to 53 and larger then 53 charecters use bigint
+let age = 46 ;// number
+let isLogedIn = true; // Boolean
+let state; // undefined
+let use = null;// standalon value
+// symble unique
+// Object 
+
+console.table(typeof[UserName, age, isLogedIn, state, use])
