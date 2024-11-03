78d783762711c8236736eb8140210103b1906f90
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-03 20:28:55 +0000

    datatype_summery

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -13,10 +13,44 @@
 
 //  console.log(IsKalidBoolean);
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
+// let stringNumber = String(someNumber);
 
-console.log(typeof(stringNumber));
+// console.log(typeof(stringNumber));
 
- 
+//  ---------------------Operations--------------------------------
+
+// let value = 3;
+
+// let negValue = -value;
+// console.log(negValue);
+// let  str1 = "Hello";
+// let str2 = " World!";
+// let str3 = str1 + str2;
+
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
---
+++ 01_Basics/datatypes-summery.js
@@ -0,0 +1,50 @@
+//(1) primative data types there (7)seven data types and there are Call by value data types.
+
+// 1-String
+// 2-Number
+// 3-Boolean
+// 4-null
+// 5-undefined
+// 6-Symbol when you create unique
+// 7-BigInt
+const userName = "khalid"
+const score = 100;
+const scoreValue = 15.2;
+const isLogedIn = false;
+const tempratue = null;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+const bigNumber = 15151516516515n;
+console.log(bigNumber);
+
+
+
+
+
+// (2) Reference type (Non-primtive data types) it can be directly allocate its reference
+// 1-Array
+// 2-Object
+// 4-Function
+
+const heros = ["khalid", "Bilal", "Faysal", "hamza"]
+
+const myObj = {
+    name: "khalid",
+    age: 45,
+    adress: "Islamabad"
+}
+
+const myFunciton = function(){
+    console.log("Hello world!");
+    
+
+}
+console.log(typeof(id));
+
+
+
+
+
+
