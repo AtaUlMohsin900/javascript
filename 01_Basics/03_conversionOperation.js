208b69c9cb83460d8f1cb29ec27b168d69902aad
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-03 18:28:48 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = true
+
+
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+
+
+//  let khalid = 1
+
+//  let IsKalidBoolean = Boolean(khalid);
+
+//  console.log(IsKalidBoolean);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof(stringNumber));
+
+ 
