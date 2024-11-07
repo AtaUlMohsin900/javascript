269cb23ea0c0ce7931fe4353ed9b7ebed4cdf187
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-07 10:57:52 +0000

    num_math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+     const score = 400;
+     const balance = new Number(100);
+
+    // console.log(balance.toString().length);
+    // console.log(typeof(balance));
+    // console.log(balance.toFixed(3));
+    
+    const otherNumber = 23.5456
+    
+    // console.log(otherNumber.toPrecision(2));
+    const hunderads = 10000000;
+    // console.log(hunderads.toLocaleString());
+    // console.log(hunderads.toLocaleString("en-PK"));
+
+    // *************Maths**************
+
+//     console.log(Math);
+//     console.log(Math.abs(-5));
+//     console.log(Math.round(4.8));
+//     console.log(Math.max(2,4,6,8,10,12));
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+console.log();
+
+    
+    
+    
+
+
+
+    
+    
+
