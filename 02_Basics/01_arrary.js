0594f8cef18b2287a3a2a2b5ceb800f6f870adbb
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-13 18:34:06 +0000

    slice_splice

--- 02_Basics/01_arrary.js
+++ 02_Basics/01_arrary.js
@@ -1,10 +1,49 @@
 //  Array
 
 const myArr = ["A","B","C","D","E"]
-const myCourses = ["JavaScript", "Python"]
-const myArr2 = new Array(1,2,3,4,5)
-console.log(myArr[2]);
-console.log(myArr2[0]);
-console.log(myCourses[1]);
+// const myCourses = ["JavaScript", "Python"]
+// const myArr2 = new Array(1,2,3,4,5)
+// access
+// console.log(myArr[2]);
+// console.log(myArr2[0]);
+// console.log(myCourses[1]);
+// update
+// myCourses[0] = "Typescript"
+// console.log(myCourses);
+// length
+// console.log(myArr2.length)
+
+// Methods of Array
+
+// myArr.push("F")
+
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(2)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes("E"))
+// console.log(myArr.indexOf("E"));
+
+// const newArr = myArr.join()
+// console.log(typeof newArr);
+
+// Slice and Splice
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+const myn2 = myArr.splice(1, 3)
+console.log("C",myArr);
+
+console.log(myn2);
+
+
+
+
 
 
