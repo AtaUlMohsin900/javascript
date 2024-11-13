31f104ffd878fd19b42003af09c970e826de1cec
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-13 03:48:37 +0000

    array

---
+++ 02_Basics/01_arrary.js
@@ -0,0 +1,10 @@
+//  Array
+
+const myArr = ["A","B","C","D","E"]
+const myCourses = ["JavaScript", "Python"]
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr[2]);
+console.log(myArr2[0]);
+console.log(myCourses[1]);
+
+
