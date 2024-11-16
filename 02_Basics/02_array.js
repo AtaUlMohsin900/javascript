53f56963b28f1d74baf7246b53303d569bc65496
Ataul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-16 07:12:33 +0000

    updated

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -9,18 +9,20 @@ const courseBooks = ["Javascript","Python", [".Net", "Roby",["CSS","HTML"]] ]
 // const  allNewBooks = [...books, ...courseBooks]
 // console.log(allNewBooks);
 
-const moreCourses = courseBooks.flat(Infinity)
-console.log(moreCourses);
-console.log(Array.isArray("khalid"));
-console.log(Array.from("khalid"));
-console.log(Array.from({
-    name: "khalid",
-    age:46,
-    address:"islamabad" 
-}));
-let score1 = 100
-let score2 = 200
-let score3 = 300
-console.log(Array.of(score1, score2, score3));
+// const moreCourses = courseBooks.flat(Infinity)
+// console.log(moreCourses);
+// console.log(Array.isArray("khalid"));
+// console.log(Array.from("khalid"));
+// console.log(Array.from({
+//     name: "khalid",
+//     age:46,
+//     address:"islamabad" 
+// }));
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+let newCourses = courseBooks
+ console.log(Array.from(newCourses));
 
 
+console.log(`Courses are here ${courseBooks.push("Rust")}`);
