967f9713e2183943a80f9c2499cb4a8b899fbdbb
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-20 03:40:33 +0000

    nested_objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -2,4 +2,31 @@
 // object.constructor
 
 const newUser = new Object()
-console.log(newUser);
+newUser.id =  "khalid123"
+newUser.name = "khlalid"
+newUser.isLogedIn = true
+// console.log(newUser);
+const regularUser = {
+    userOneName: {
+        firstName: "ataul moshin",
+        lastName: "khalid",
+        email: "abc@gamil.com",
+    otherInfo : {
+        adress:"Islamabad",
+        Ocupassion: "Bank Empolyee",
+        Qualification:"Bachlor in IT",
+        }
+    },
+    userTwoName: {
+        firstName: "Muhammad",
+        lastName: "uzair",
+        email: "asdf@gamil.com",
+    otherInfo : {
+        adress:"Islamabad",
+        Ocupassion: "Developer in IBM",
+        Qualification:"Bachlor in Computer Science",
+        }
+    }
+}
+console.log(regularUser.userOneName.otherInfo.Qualification);
+
