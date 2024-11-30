c763494e76a72a944b880b6f4cf6104b522d4277
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-30 06:29:23 +0000

    function_storeIn_variable

--- 03_Basics/functions.js
+++ 03_Basics/functions.js
@@ -16,10 +16,17 @@ function twoValue(value1, value2){
     console.log(value1+value2);
     
 }
-// twoValue(8,5)
+// twoValue(8,null)
 
 function addTwoStrings(string1, string2){
     console.log(string1+string2);
     
 }
 // addTwoStrings("Ataul", "Mohsin")
+
+function twoValue(value1, value2){
+    console.log(value1+value2);
+    
+}
+const answer = twoValue(8,8)
+console.log("Our Answer is:",answer);
