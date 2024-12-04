cf4bb6f030f8d8bbfdf511d3b6abd1e6ca1ad1e4
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-04 15:57:13 +0000

    scope_in_function

--- 03_Basics/scopes.js
+++ 03_Basics/scopes.js
@@ -12,27 +12,30 @@ if(true){
 
 
 // Nested Scope
-function main(){
-    const mainName = "Khalid"
-  function nested(){
-    const nestedName = "Mohsin"
-    console.log(mainName)
-}
-// console.log(nestedName);
 
-// nested()
-
-}
-// main()
-
-if(true){
-    const username = "khalid"
-    if(username === "khalid"){
-        const newuser = " Malik"
-        console.log(username+ newuser);
+function first(){
+    const parent = "khalid"
+    console.log(parent)
+    function second(){
+        const child = "uzair"
+        console.log(child);
         
     }
-    // console.log(newuser)
+    first()
 }
+
+// second()
+
+
+
+// if(true){
+//     const username = "khalid"
+//     if(username === "khalid"){
+//         const newuser = " Malik"
+//         console.log(username+ newuser);
+        
+//     }
+//     // console.log(newuser)
+// }
 
 // console.log(newuser);
