94b72f93ff7e5fafb2b2cc51947f248fb97b9133
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-04 09:51:27 +0000

    updated

--- 03_Basics/scopes.js
+++ 03_Basics/scopes.js
@@ -3,10 +3,34 @@ if(true){
     let name1 = "Javed"
     const name2 = "Hashim"
     // var name3 = "umer";
-    console.log("block_scope:",name1)
+    // console.log("block_scope:",name1)
     
 }
-console.log("global_scope:",name1);
+// console.log("global_scope:",name1);
 // console.log(name2);
 // console.log(name3);
 
+
+// Nested Scope
+function main(){
+    const mainName = "Khalid"
+  function nested(){
+    const nestedName = "Mohsin"
+    console.log(mainName)
+}
+// console.log(nestedName);
+
+// nested()
+
+}
+// main()
+
+if(true){
+    const username = "khalid"
+    if(username === "khalid"){
+        const newuser = " Malik"
+        console.log(username+ newuser);
+        
+    }
+    console.log(newuser)
+}
