e5532dc3e220a0473dca3a78c426adf14ec9c6e5
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-05 16:21:13 +0000

    arrow_oneline

--- 03_Basics/arrow_funcitons.js
+++ 03_Basics/arrow_funcitons.js
@@ -5,9 +5,32 @@ const client = {
         console.log(`${this.clientName}, You are successfully join our team`);
         
     }
+    
 
 }
 
-client.message4client()
-client.clientName ="Mohsin"
-client.message4client()
+// client.message4client()
+// client.clientName ="Mohsin"
+// client.message4client()
+// console.log(this)
+
+// function newFun(){
+//    console.log(this);
+    
+// }
+// newFun()
+
+let addnum1 = (num1, num2) => {
+    return num1+num2
+    
+}
+
+console.log(addnum1(4,4))
+
+let addnum2 = (num1, num2) => num1+num2
+console.log(addnum2(4,5))
+
+let addnum3 = (num1, num2) => (num1+num2)
+
+    console.log(addnum3(4,6))
+
