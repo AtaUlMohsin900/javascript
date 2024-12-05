5e00c37c39b0b98ecdff536f13749709ec6fa6e3
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-05 13:43:51 +0000

    first_code

---
+++ 03_Basics/arrow_funcitons.js
@@ -0,0 +1,10 @@
+const student = {
+    studentName: "Mohsin",
+    rollNubmer: 945,
+    greetingMessage: function(){
+        console.log(`${this.studentName}, Welcome to Student Portel`)
+
+        
+    }
+}
+student.greetingMessage()
