19a453b658894aacd008aa6d4db005ece65c9df7
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-07 08:04:59 +0500

    Update iife.js

--- 03_Basics/iife.js
+++ 03_Basics/iife.js
@@ -4,26 +4,21 @@
 // simple function
 function newFun(){
     console.log(`Database now connected`)
-
 }
 newFun();
+
 
 // iife function
 (function iifeFun(){    //named iife
     console.log(`Database is connected now`)
-
 })();
 
 // iife with arrow function
 (  iifeArrow = () =>{   //simple iife
     console.log(`Database is connected using iife_arrow function`);
-    
-
 } )();
 
 // another example
 (  iifeVariable = (value) =>{
     console.log(`Database is connected ${value}`);
-    
-
 } )("khalid")
