ef19f544315e714e3f5a9a045ed26de9002b9abe
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-07 14:15:57 +0000

    update

--- 03_Basics/highOrderFunctions.js
+++ 03_Basics/highOrderFunctions.js
@@ -1,10 +1,12 @@
 // A function that takes a function as an argument is called High Oder Function
-function add(a, b, callback){
-    let answer = a+b;
-    callback(answer)
+function add(a, b, newfun){
+    let answer = a + b;
+    newfun(answer)
 }
 // function showAnswer(answer){
 //     console.log(answer);
 // }
 // add(2,4,showAnswer)
-add(2,4,(value)=>console.log(value))
+add(2,4,(val)=>console.log(val))
+// add(2,4, value =>console.log(value))
+add(100, 20, (res)=>console.log(res))
