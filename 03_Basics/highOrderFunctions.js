7f21e169c2174654b113cc688bb2ec068b1d52c1
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-07 14:23:27 +0000

    update

--- 03_Basics/highOrderFunctions.js
+++ 03_Basics/highOrderFunctions.js
@@ -2,11 +2,15 @@
 function add(a, b, newfun){
     let answer = a + b;
     newfun(answer)
+    return ()=> console.log(answer);
+    
 }
 // function showAnswer(answer){
 //     console.log(answer);
 // }
 // add(2,4,showAnswer)
-add(2,4,(val)=>console.log(val))
+// add(2,4,(val)=>console.log(val))
 // add(2,4, value =>console.log(value))
-add(100, 20, (res)=>console.log(res))
+// add(100, 20, (res)=>console.log(res))
+let anotherFun = add(2,4,()=>{})
+anotherFun()
