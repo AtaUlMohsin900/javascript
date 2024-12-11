0b66c3ce42fe03b04f7411f82988aed7fe5359fe
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-11 17:12:14 +0000

    for_loop

---
+++ 03_Basics/loops.js
@@ -0,0 +1,11 @@
+// Loops are used to execute a peice of code again and again.
+
+// syntax of for loop it has Three statements 
+// 1. initialize statement
+// 2. running length of loop is second statement
+// 3. updatation statement
+
+for(let i = 0; i<=10; i++){
+    console.log(i);
+    
+}
