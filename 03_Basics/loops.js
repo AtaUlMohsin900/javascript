15a67aa92555aec9aaa2c59a453fa9101213069d
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-12 18:39:21 +0000

    table using while loop

--- 03_Basics/loops.js
+++ 03_Basics/loops.js
@@ -23,11 +23,28 @@
 
 // create a Table using for loop
 
-for(let i = 1; i<=10; i++){
-    console.log(`2 x ${i} = ${i*2}`);
+// for(let i = 1; i<=10; i++){
+//     console.log(`2 x ${i} = ${i*2}`);
 
     
+// }
+// console.log("Table Complete");
+
+// Do'nt use any infinite loop
+
+// While loop in JavaScript
+
+let username = 0;
+while (username <= 10){
+    console.log("khalid");
+    username++
+    
 }
-console.log("Table Complete");
 
-
+// create a Table using while loop
+let i = 1;
+while (i <= 10){
+    console.log(`2 x ${i} = ${i*2}`);
+    i++
+    
+}
