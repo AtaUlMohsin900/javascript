c77efc7d4cd97cab23493fb257301aa9267d3415
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-29 15:01:41 +0000

    example

--- 03_Basics/callstack.js
+++ 03_Basics/callstack.js
@@ -5,3 +5,13 @@
 //  code+{} -> code in two phases
 // 1- Memory creation phase
 // 2- Execution phase
+// Example
+let num1 = 5;
+let num2 = 7;
+function addNum(num1,num2){
+    let answer = num1+num2;
+    return answer
+}
+let result1 = addNum(num1,num2);
+let result2 = addNum(10,7);
+
