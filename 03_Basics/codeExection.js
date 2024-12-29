89f08673ebac18da889bf5f82aec0782932d30a8
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-29 15:59:23 +0000

    2nd_cycle

--- 03_Basics/codeExection.js
+++ 03_Basics/codeExection.js
@@ -5,7 +5,10 @@
 //  code+{} -> code in two phases
 // 1- Memory creation phase
 // 2- Execution phase
+
+
 // Example
+// Momory phase
 let num1 = 5;
 let num2 = 7;
 // num1 and num2  in Memory execution and its value is Undefined
@@ -17,3 +20,18 @@ let result1 = addNum(num1,num2); // undefined
 
 let result2 = addNum(10,7); // undefined
 
+// 1st cycle (Memory phase) completed
+
+// Execution phase
+let num3 = 5;
+let num4 = 7;
+// num3 and num4  in Execution and its values are 5, 7 
+function addNum(num1,num2){  // Defination
+    let answer = num1+num2;
+    return answer
+}
+let result3 = addNum(num1,num2); // undefined
+
+let result4 = addNum(10,7); // undefined
+
+// 2st cycle (Execution phase) completed
