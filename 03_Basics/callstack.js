be84609260371de3f7f40bd741d51f64446b53ec
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-12-29 14:54:51 +0000

    Execution_phases

--- 03_Basics/callstack.js
+++ 03_Basics/callstack.js
@@ -1,2 +1,7 @@
 // Javascript Execution Context
-// code{} ->Global Execution Context placed in "this"
+// code{} ->   Global Execution Context      placed in "this" and in browser value should be in window object
+// code+{} ->  Function Exectution Context
+// code+{} ->  Eval Exectution Context
+//  code+{} -> code in two phases
+// 1- Memory creation phase
+// 2- Execution phase
