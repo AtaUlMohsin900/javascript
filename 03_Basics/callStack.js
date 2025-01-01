c07e25060b7620f1a7e908a64f92a5fc3445058a
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-01 06:27:46 +0000

    explainaiton

---
+++ 03_Basics/callStack.js
@@ -0,0 +1,5 @@
+/* Call Stack in JavaScript is a data structure that keeps track of function calls in the program. 
+It follows a Last In, First Out (LIFO) principle, meaning the last function added to the stack is the 
+first one to be executed. When a function is invoked, it's pushed onto the stack. Once the function completes,
+ it's popped off the stack.
+*/
