76d18ac630ff83003a85ee987a130e68f261325d
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-01 06:28:46 +0000

    examples

--- 03_Basics/callStack.js
+++ 03_Basics/callStack.js
@@ -3,3 +3,24 @@ It follows a Last In, First Out (LIFO) principle, meaning the last function adde
 first one to be executed. When a function is invoked, it's pushed onto the stack. Once the function completes,
  it's popped off the stack.
 */
+// example
+
+// go to chrome browser open inspect and go to source ->create one file abc.paste the following code
+// js check call stack and run this code
+// press enter button check out one by one.
+
+function first(){
+    console.log("first");
+    second()
+
+}
+function second(){
+    console.log("second");
+    third()
+}
+function third(){
+    console.log("third");
+}
+first()
+second()
+third()
