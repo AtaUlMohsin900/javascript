bc61a91bf2521873d0281addec8a6aff89582ca5
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-01 18:31:00 +0000

    about_control_flow

---
+++ 04_LogicFlow/controlFlow.js
@@ -0,0 +1,6 @@
+/* Control flow in JavaScript refers to the order in which individual statements, instructions, or functions are 
+executed in a script. By default, JavaScript executes code from top to bottom. However, control flow can be altered 
+using conditional statements like `if`, `else if`, and `else` to execute specific blocks of code based on conditions.
+ Loops like `for`, `while`, and `do...while` allow repeating tasks until a certain condition is met. Additionally,
+  keywords like `break`, `continue`, and `return` provide finer control over the execution flow, while `try...catch`
+   handles exceptions to manage errors gracefully.*/
