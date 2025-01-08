5c4b634c88df6b4b2fcbf2b8df091aff0192b258
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-08 20:54:39 +0000

    channing_with_filter

--- 05_intermediate/map.js
+++ 05_intermediate/map.js
@@ -6,5 +6,6 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const newNums = myNums
                 .map((item)=> item *  10)
                 .map((item)=> item /  2)
+                .filter((item)=>item >= 35 )
 
 console.log(newNums);
