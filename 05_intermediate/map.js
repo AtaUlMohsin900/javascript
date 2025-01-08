26a67ba36ad09102b5dfe67ad251005b2e20b4b9
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-08 20:41:43 +0000

    commit

--- 05_intermediate/map.js
+++ 05_intermediate/map.js
@@ -1,5 +1,5 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNums.map((item)=> item + 10)
+const newNums = myNums.map((item)=> item + 10) // we can use forEach loop instead of map
 
 console.log(newNums);
