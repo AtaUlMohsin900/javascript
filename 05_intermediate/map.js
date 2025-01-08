3dad831cbbaed7204f582e07d59dadd2dc0bbcab
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-08 20:50:49 +0000

    channing

--- 05_intermediate/map.js
+++ 05_intermediate/map.js
@@ -1,5 +1,10 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNums.map((item)=> item + 10) // we can use forEach loop instead of map
+// const newNums = myNums.map((item)=> item + 10) // we can use forEach loop instead of map
+
+// Map with channing
+const newNums = myNums
+                .map((item)=> item *  10)
+                .map((item)=> item /  2)
 
 console.log(newNums);
