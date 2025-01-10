3a317932598de3969761e33f07c0ce8ebe34456c
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-10 18:06:46 +0000

    update

--- 05_intermediate/reduce.js
+++ 05_intermediate/reduce.js
@@ -1,8 +1,13 @@
 const myNumbers = [1, 2, 3];
-const numsTotal = myNumbers.reduce(function(accumalater, currentVal){
-    console.log(`accumalater value: ${accumalater} and current value: ${currentVal}`);
+// const numsTotal = myNumbers.reduce(function(accumalater, currentVal){
+    // console.log(`accumalater value: ${accumalater} and current value: ${currentVal}`);
     
-    return accumalater + currentVal
+    // return accumalater + currentVal
+
+// },0)
+// console.log(numsTotal);
+
+// Example with arrow funciton
+const numsTotal = myNumbers.reduce((accumalater, currentVal)=>{
 
 },0)
-console.log(numsTotal);
