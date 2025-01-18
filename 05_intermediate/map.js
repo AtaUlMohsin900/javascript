ce47ad4aef70e0b7c7cbff5848a68e7a81c97c47
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2025-01-18 19:10:28 +0500

    q&a

--- 05_intermediate/map.js
+++ 05_intermediate/map.js
@@ -4,8 +4,8 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // Map with channing
 const newNums = myNums
-                .map((item)=> item *  10)
-                .map((item)=> item /  2)
-                .filter((item)=>item >= 35 )
+    .map((item) => item * 10)
+    .map((item) => item / 2)
+    .filter((item) => item >= 35)
 
 console.log(newNums);
