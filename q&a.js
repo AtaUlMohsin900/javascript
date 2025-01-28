f8218f310bc9a52a0d9feb24740ccf7c91c9dd64
ataulmohsin900@gmail.com <ataulmohisn900@gmail.com>
2025-01-28 09:28:11 +0500

    IEEE 754

---
+++ q&a.js
@@ -0,0 +1,15 @@
+// const a = {};
+// const b = { key: "b" };
+// const c = { key: "c" };
+// a[b] = 123;
+// a[c] = 456;
+// console.log(a[b]);
+
+// Standard IEEE 754
+if (0.1 + 0.2 === 0.3) {
+    console.log(true);
+
+} else {
+    console.log(false);
+
+}
