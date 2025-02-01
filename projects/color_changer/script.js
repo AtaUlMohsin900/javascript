7b2754e6a8308ebc25721da2757baa5b0b634671
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-02-01 06:44:28 +0000

    update

--- projects/color_changer/script.js
+++ projects/color_changer/script.js
@@ -5,6 +5,9 @@ const body = document.querySeclector("body")
 buttons.forEach( function(button){
     console.log(button);
     button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        
 
     })
     
