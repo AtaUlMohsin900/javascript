3975ccba9af6d6e3548667ba025c053ce2b60f0d
ataulmohsin900@gmail.com <ataulmohisn900@gmail.com>
2025-02-01 12:11:24 +0500

    chage

--- projects/color_changer/script.js
+++ projects/color_changer/script.js
@@ -11,6 +11,18 @@ buttons.forEach(function (button) {
         if (e.target.id === 'gray') {
             body.style.backgroundColor = e.target.id;
         }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'purple') {
+            body.style.backgroundColor = e.target.id;
+        }
 
 
 
