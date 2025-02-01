f781e2f4ab25e3179a734c70285eec3c5e15cf0f
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-02-01 06:25:49 +0000

    eventListner

--- projects/color_changer/script.js
+++ projects/color_changer/script.js
@@ -2,8 +2,11 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySeclector("body")
 
 
-buttons.forEach(function(button){
+buttons.forEach( function(button){
     console.log(button);
+    button.addEventListener('click', function(e){
+
+    })
     
 })
     
