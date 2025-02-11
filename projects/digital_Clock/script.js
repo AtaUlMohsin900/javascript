ac9157d239b32deb87df4b894565fe1b7a3b29cd
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-02-11 00:42:27 +0000

    time_project

--- projects/digital_Clock/script.js
+++ projects/digital_Clock/script.js
@@ -1,2 +1,11 @@
-const clock =document.getElementById('clock')
+const clock = document.getElementById('clock')
 // const clock =document.querySelector('#clock')
+
+
+
+setInterval(function(){
+    let date = new Date()
+// console.log(date.toLocaleTimeString())
+clock.innerHTML = date.toLocaleTimeString();
+
+},1000)
