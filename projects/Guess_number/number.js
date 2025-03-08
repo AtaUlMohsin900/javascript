e596b68ad917203f86e91a05834ad9f4c48750c6
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-03-08 00:39:57 +0000

    update

--- projects/Guess_number/number.js
+++ projects/Guess_number/number.js
@@ -2,4 +2,12 @@ const randomNumber = (parseInt(Math.random() *100 +1))
 
 const submit = document.querySelector('#subt')
 const userInput = document.querySelector('#guessField')
+const guesSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const loworhigh = document.querySelector('.loworhigh')
+const startOver = document.querySelector('.resultParas')
+
+
+
+
 
