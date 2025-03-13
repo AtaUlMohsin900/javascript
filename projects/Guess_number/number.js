dabc323f2a3c2b8acf59de07efab376bd6d71273
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-03-13 16:24:39 +0000

    validate_guess

--- projects/Guess_number/number.js
+++ projects/Guess_number/number.js
@@ -16,6 +16,15 @@ let numGuess = 1;
 
 let playGame = true;
 
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+       const guess = parseInt(userInput.value)
+       validateGuess(guess)
+
+    })
+}
+
 function validateGuess(guess){
 
 }
@@ -24,7 +33,19 @@ function checkGuess(guess){
 
 }
 
+function displayGuess(guess){
+
+}
+
 function    displayMessage(message){
+
+}
+
+function endGame(){
+
+}
+
+function newGame(){
 
 }
 
