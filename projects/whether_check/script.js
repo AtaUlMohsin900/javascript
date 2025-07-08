32e6a23117e412f6d0b68116b642f8af58d7fd14
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2025-07-08 20:58:24 +0500

    update

--- projects/whether_check/script.js
+++ projects/whether_check/script.js
@@ -1,1 +1,6 @@
-alert("Hello, World!");
+const button = document.getElementById('search_button');
+const input = document.getElementById('city-input');
+
+button.addEventListener('click', ()=> {
+   alert(input.value);
+})
