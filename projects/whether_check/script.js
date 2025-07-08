bbf3c60e0f028103e5cd0757da9995689277b3ef
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2025-07-08 21:09:50 +0500

    update

--- projects/whether_check/script.js
+++ projects/whether_check/script.js
@@ -2,5 +2,9 @@ const button = document.getElementById('search_button');
 const input = document.getElementById('city-input');
 
 button.addEventListener('click', ()=> {
-   alert(input.value);
-})
+   console.log(input.value);
+
+});
+// Go to Free api like https://www.weatherapi.com/ copy the api key and opnen another side or api explorer and paste it here which page required.
+// scroll down and change the option no to yes under the value option.
+// copy the call section link and paste it on URL bar.
