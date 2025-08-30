34687d80ec8f1ae0c26b9b848e24f47c3f174a78
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-08-30 04:39:24 +0000

    make expression

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,5 +1,8 @@
 // const input = document.getElementById('numInput')
+const input = document.getElementById('numInput')
 function buttonHandler(el){
     console.log(el.innerText)
+    const exp = `${input.value} ${el.innerText}`
+    input.value = exp
 
 }
