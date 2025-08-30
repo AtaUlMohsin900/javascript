99039b1309e8cbdd9e4613e7791c81e058fd56b8
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-08-30 05:01:30 +0000

    Symbol error handling

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,11 +1,14 @@
 // const input = document.getElementById('numInput')
 const input = document.getElementById('numInput')
 const result = document.getElementById('result')
+
 function buttonHandler(el){
     console.log(el.innerText)
     const exp = `${input.value} ${el.innerText}`
     input.value = exp
+    if(['+','-','*','/'].includes(el.innerText)) return;
     result.value = eval.exp
+    
 
 }
 
