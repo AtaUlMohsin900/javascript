7a182db9ba407501f4a5e8a979bac1a7fbe24cfb
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-08-30 04:48:31 +0000

    creating Result as output

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,8 +1,10 @@
 // const input = document.getElementById('numInput')
 const input = document.getElementById('numInput')
+const result = document.getElementById('result')
 function buttonHandler(el){
     console.log(el.innerText)
     const exp = `${input.value} ${el.innerText}`
     input.value = exp
+    result.value = eval.exp
 
 }
